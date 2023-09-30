c3d9030c9902628384ce6dbad7d46a5371d3c9ff
kooroosh1363 <peymanraad1363@gmail.com>
2023-09-30 23:02:18 +0400

    add images and data list product

---
+++ src/data/data_list_product.jsx
@@ -0,0 +1,92 @@
+const list_product = [
+    {
+        id : 1,
+        name_product: "shirt 1",
+        description : "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+        price: 30,
+        currency: "$",
+        image: "../images/1.png",
+        sizes: {
+            S : "S",
+            M : "M",
+            L : "L",
+            XL: "XL"
+        }
+        
+    },
+    {
+        id : 2,
+        name_product: "shirt 2",
+        description : "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+        price: 30,
+        currency: "$",
+        image: "../images/2.png",
+        sizes: {
+            S : "S",
+            M : "M",
+            L : "L",
+            XL: "XL"
+        }
+        
+    },
+    {
+        id : 3,
+        name_product: "shirt 3",
+        description : "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+        price: 30,
+        currency: "$",
+        image: "../images/3.png",
+        sizes: {
+            S : "S",
+            M : "M",
+            L : "L",
+            XL: "XL"
+        }
+        
+    },
+    {
+        id : 4,
+        name_product: "shirt 4",
+        description : "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+        price: 30,
+        currency: "$",
+        image: "../images/4.png",
+        sizes: {
+            S : "S",
+            M : "M",
+            L : "L",
+            XL: "XL"
+        }
+        
+    },
+    {
+        id : 5,
+        name_product: "shirt 5",
+        description : "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+        price: 30,
+        currency: "$",
+        image: "../images/5.png",
+        sizes: {
+            S : "S",
+            M : "M",
+            L : "L",
+            XL: "XL"
+        }
+        
+    },
+    {
+        id : 6,
+        name_product: "shirt 6",
+        description : "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+        price: 30,
+        currency: "$",
+        image: "../images/1.png",
+        sizes: {
+            S : "S",
+            M : "M",
+            L : "L",
+            XL: "XL"
+        }
+        
+    }
+]
