428367cd0badb90933d0a3e4fd2ce551cce84728
kooroosh1363 <peymanraad1363@gmail.com>
2023-10-01 12:52:44 +0400

    add pages component and react router dom

---
+++ src/component/lista/Lists.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./Lists.module.css";
+
+
+function Lists(){
+    return(
+        <div>
+                lists
+        </div>
+    );
+};
+
+
+export default Lists;
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "./Home.module.css";
+
+function Home(){
+    return(
+        <div>
+            home
+        </div>
+    )
+}
+
+export default Home;
