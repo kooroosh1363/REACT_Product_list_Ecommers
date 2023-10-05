8ab27934b2342ef12e978fd25ccc13c31bb03090
kooroosh1363 <peymanraad1363@gmail.com>
2023-10-05 19:07:16 +0400

    add toggle menu codes

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,37 +1,65 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { BsXSquare } from "react-icons/bs";
+import { BsTextCenter } from "react-icons/bs";
 import "./Navbar.css"
 import Btn from '../button/Btn';
+import logo from "../../assets/img/raad.png"
+
+
+
 const Navbar = () => {
-  return (
-    <>
-    <nav className="navbar">
-        <div className="container dis_flex">
+    // scroll for top position
+    window.addEventListener("scroll", function () {
+        const navbar = document.querySelector(".navbar")
+        navbar.classList.toggle("active", window.scrollY > 100)
+    })
 
-            <div className="logo">
-                <img src="" alt="" />
-            </div>
+    const [Mobile, setMobile] = useState(false)
 
-            <div className="nav_link">
-                <ul className="f_flex link uppercase">
-                    <li><a href="#home">Home</a></li>
-                    <li><a href="#Features">Features</a></li>
-                    <li><a href="#Portfolio">Portfolio</a></li>
-                    <li><a href="#Resume">Resume</a></li>
-                    <li><a href="#Clients">Clients</a></li>
-                    <li><a href="#Blog">Blog</a></li>
-                    <li><a href="#Contact">Contact</a></li>
-                    <li><Btn/></li>
-                </ul>
-                
 
-                <button><BsXSquare></BsXSquare></button>
-            </div>
-        </div>
-    </nav>
-    </>
-  )
+    return (
+        <>
+            <nav className="navbar">
+                <div className="container dis_flex">
+
+                    <div className="logo">
+                        <img src={logo} alt="" />
+                    </div>
+
+                    <div className="nav_link">
+                        {/* <ul className="f_flex link uppercase"> */}
+
+                        <ul className={Mobile ? "mobile_link" : "link f_flex uppercase"} onClick={() => setMobile(false)}>
+
+                            <li><a href="#home">Home</a></li>
+                            <li><a href="#Features">Features</a></li>
+                            <li><a href="#Portfolio">Portfolio</a></li>
+                            <li><a href="#Resume">Resume</a></li>
+                            <li><a href="#Clients">Clients</a></li>
+                            <li><a href="#Blog">Blog</a></li>
+                            <li><a href="#Contact">Contact</a></li>
+                            <li><Btn /></li>
+                        </ul>
+
+
+                        <button className='toggle' onClick={() => setMobile(!Mobile)}>
+
+                            {Mobile ?
+                                <BsXSquare className='close btn_c'></BsXSquare> :
+                                <BsTextCenter className='open btn_o'></BsTextCenter>
+                            }
+
+
+                        </button>
+                    </div>
+                </div>
+            </nav >
+
+
+            <section className="demo"></section>
+        </>
+    )
 }
 
 export default Navbar
