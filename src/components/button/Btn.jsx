ea913c0c9b8392e9b427dc006c8d5baad0307ad8
kooroosh1363 <peymanraad1363@gmail.com>
2023-10-05 11:32:34 +0400

    add new css codes and btn component

---
+++ src/components/button/Btn.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./Btn.css"
+const Btn = () => {
+  return (
+    <>
+      <button className='btn_h'>BUY NOW</button>
+    </>
+  )
+}
+
+export default Btn
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Home.css"
+import { Nav } from 'react-bootstrap'
+import Navbar from '../../components/navbar/Navbar'
+
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default Home
