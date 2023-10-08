02ab8ca60df3a2f90e916c2faa9a9f178c4d1ee6
kooroosh1363 <peymanraad1363@gmail.com>
2023-10-08 19:46:02 +0400

    add new code for feature component

--- src/components/Feature/Feature.jsx
+++ src/components/Feature/Feature.jsx
@@ -1,10 +1,35 @@
 import React from 'react'
-
+import { FaArrowRight } from "react-icons/fa";
+import "./Feature.css"
 const Feature = () => {
   return (
-    <div>
-      <h1>feature</h1>
-    </div>
+    <>
+      <section>
+        <div className="top feature" id="Features">
+          <div className="container">
+            <div className="heading">
+              <h4>Features</h4>
+              <h1>What I Do</h1>
+            </div>
+
+            <div className="content grid">
+              <div className="box ">
+                <img src="" alt="" />
+
+                <h2 className='title'>Title</h2>
+
+                <p className='paraph'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, expedita itaque. Reiciendis, tempore distinctio voluptatibus odit tempora consequuntur quod nisi.</p>
+
+                <a href="#">
+                  <FaArrowRight className='arrow_right'/>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+    </>
   )
 }
 
